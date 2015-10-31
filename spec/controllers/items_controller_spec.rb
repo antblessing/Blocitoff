1f9246567736db20ed0e3f37b9b52e8b1b4d7e0e
antblessing <antblessing@gmail.com>
2015-10-31 01:55:20 +0000

    ajax delete

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
 
-  describe "GET #create" do
+  describe "POST #create" do
     it "returns http success" do
       user = create(:user)
-      item = create(:item)
       sign_in user
+      item = user.Item.new
       post :create
       expect(response).to have_http_status(:success)
     end
