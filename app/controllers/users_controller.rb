067355c25e3cb30df87c112a0f3428aee13a86d5
antblessing <antblessing@gmail.com>
2015-11-03 21:49:17 +0000

    extra

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
   
+  before_filter :verify_owner
+  
   def show
+    
+  end
+  
+  private
+  
+  def verify_owner
     @user = User.find(params[:id]) if params[:id]
     @user ||= current_user
+    
+    redirect_to welcome_about_path unless @user == current_user
   end
 end
